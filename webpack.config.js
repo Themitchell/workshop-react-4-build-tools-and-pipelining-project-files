758008702cec39f4ea5a15b94ae82f6affec59e9
Andy Mitchell <326561+Themitchell@users.noreply.github.com>
2023-06-05 13:50:17 +0100

    Step 3 - Add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -6,5 +7,19 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.bundle.js'
-  }
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: 9000,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join('src', 'index.template.html'),
+      inject: 'body'
+    })
+  ]
 };
